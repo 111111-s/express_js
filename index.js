f2689601366382a44ca6535af07e597ddec3a5f6
111111-s <yasinsky999@gmail.com>
2021-02-27 21:48:56 +0200

    Big update
    fix bug and add local storage

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const express = require("express");
-const exphbs = require("express-handlebars")
-const path = require("path")
-
-const Homeroutes = require('./routes/home')
-const Testroutes = require('./routes/test')
-const Inforoutes = require('./routes/info')
-
-
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 const app = express()
+
 const hbs = exphbs.create({
-    defaultLayout: "main",
-    extname:"hbs"
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
-app.engine("hbs",hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views','views')
-app.use(express.urlencoded({extended:true}))
-app.use("/",Homeroutes)
-app.use("/test",Testroutes)
-app.use("/info",Inforoutes)
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
+const PORT = process.env.PORT || 3000
 
-const PORT = process.env.PORT || 3008
 app.listen(PORT, () => {
-    console.log(`server starting on port: ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
-
